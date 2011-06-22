fc7a5ca7b48f32656c6110667e126f204370040c
Dan Prince <dan.prince@rackspace.com>
2011-06-21 20:53:46 -0400

    Added a poll_vpn_interface method to poll for the tap/tun to be created.

--- lib/chef-vpc-toolkit/vpn_openvpn.rb
+++ lib/chef-vpc-toolkit/vpn_openvpn.rb
@@ -2,16 +2,16 @@
 module ChefVPCToolkit
 class VpnOpenVpn < VpnConnection
 
-		def initialize(group, client = nil)
-			super(group, client)
-		end
+	def initialize(group, client = nil)
+		super(group, client)
+	end
 
-		def connect
-				create_certs
+	def connect
+		create_certs
 
-				@up_script=get_cfile('up.bash')
-				File.open(@up_script, 'w') do |f|
-						f << <<EOF_UP
+		@up_script=get_cfile('up.bash')
+		File.open(@up_script, 'w') do |f|
+				f << <<EOF_UP
 #!/bin/bash
 
 # setup routes
@@ -23,20 +23,20 @@ egrep ^search /etc/resolv.conf.bak | sed -e 's/search /search #{@group.domain_na
 echo 'nameserver #{@group.vpn_network.chomp("0")+"1"}' >> /etc/resolv.conf
 grep ^nameserver /etc/resolv.conf.bak >> /etc/resolv.conf
 EOF_UP
-						f.chmod(0700)
-				end
-				@down_script=get_cfile('down.bash')
-				File.open(@down_script, 'w') do |f|
-						f << <<EOF_DOWN
+			f.chmod(0700)
+		end
+		@down_script=get_cfile('down.bash')
+		File.open(@down_script, 'w') do |f|
+				f << <<EOF_DOWN
 #!/bin/bash
 mv /etc/resolv.conf.bak /etc/resolv.conf
 EOF_DOWN
-					f.chmod(0700)
-				end
+			f.chmod(0700)
+		end
 
-				@config_file=get_cfile('config')
-				File.open(@config_file, 'w') do |f|
-						f << <<EOF_CONFIG
+		@config_file=get_cfile('config')
+		File.open(@config_file, 'w') do |f|
+			f << <<EOF_CONFIG
 client
 dev #{@group.vpn_device}
 proto tcp
@@ -65,33 +65,47 @@ verb 3
 up #{@up_script}
 down #{@down_script}
 EOF_CONFIG
-					f.chmod(0600)
-				end
+			f.chmod(0600)
+		end
 
-			disconnect if File.exist?(get_cfile('openvpn.pid'))
-			out=%x{sudo openvpn --config #{@config_file} --writepid #{get_cfile('openvpn.pid')} --daemon}
-			retval=$?
-			if retval.success? then
-				puts "Openvpn started."
-			else
-				raise "Failed to create VPN connection: #{out}"
-			end
+		disconnect if File.exist?(get_cfile('openvpn.pid'))
+		out=%x{sudo openvpn --config #{@config_file} --writepid #{get_cfile('openvpn.pid')} --daemon}
+		retval=$?
+		if retval.success? then
+			poll_vpn_interface
+			puts "OK."
+		else
+			raise "Failed to create VPN connection: #{out}"
+		end
 	end
 
-		def disconnect
-			raise "Not running? No pid file found!" unless File.exist?(get_cfile('openvpn.pid'))
-			pid = File.read(get_cfile('openvpn.pid')).chomp
-			system("sudo kill -TERM #{pid}")
-			File.delete(get_cfile('openvpn.pid'))
-		end
+	def disconnect
+		raise "Not running? No pid file found!" unless File.exist?(get_cfile('openvpn.pid'))
+		pid = File.read(get_cfile('openvpn.pid')).chomp
+		system("sudo kill -TERM #{pid}")
+		File.delete(get_cfile('openvpn.pid'))
+	end
 
-		def connected?
-			system("/sbin/route -n | grep #{@group.vpn_network.chomp("0")+"1"} &> /dev/null")
-		end
+	def connected?
+		system("/sbin/route -n | grep #{@group.vpn_network.chomp("0")+"1"} &> /dev/null")
+	end
 
-		def clean
-			delete_certs
+	def clean
+		delete_certs
+	end
+
+	private
+	def poll_vpn_interface
+		interface_name=@group.vpn_device+"0"
+		1.upto(30) do |i|
+			break if system("ifconfig #{interface_name} &> /dev/null")
+			if i == 30 then
+				disconnect
+				raise "Failed to connect to VPN."
+			end
+			sleep 0.5
 		end
-private
+	end
+
 end
 end
